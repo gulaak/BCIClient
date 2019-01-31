7499aa85adb0959ba36c3dd1cd1c12e3d1c8705b
Dylan Souza <dylan_anthony@live.com>
2019-01-31 16:18:33 -0500

    sort of serializes maybe

--- src/packages/Scenes.java
+++ src/packages/Scenes.java
@@ -1,8 +1,25 @@
 package packages;
 
-import java.io.Serializable;
+import java.io.*;
+
+import javafx.scene.control.ToggleGroup;
 
 public class Scenes implements Serializable {
+	
+	
+	private static final long serialVersionUID = 4586;
+	
+	private transient ToggleGroup buttonStatus;
+	private int sliderStatus;
+	
+	
+	public Scenes(ToggleGroup a, int b)
+	{
+		buttonStatus = a;
+		sliderStatus = b;
+	}
+	
+	
 	
 	
 	
